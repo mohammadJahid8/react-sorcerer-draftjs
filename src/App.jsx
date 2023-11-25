9c3a3304be271b2c680f7560666883c444ff6870
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-11-25 10:41:36 +0600

    fixing key input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 // src/Editor.js
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import {
   Editor,
   EditorState,
   RichUtils,
   getDefaultKeyBinding,
-  convertToRaw,
   convertFromRaw,
   CompositeDecorator,
 } from "draft-js";
@@ -31,6 +30,7 @@ const redColorDecorator = {
 const compositeDecorator = new CompositeDecorator([redColorDecorator]);
 
 const App = () => {
+  const [currentText, setCurrentText] = useState();
   const [editorState, setEditorState] = useState(() => {
     // Load content from localStorage on component mount
     const savedContent = localStorage.getItem("editorContent");
@@ -41,12 +41,12 @@ const App = () => {
     return EditorState.createEmpty(compositeDecorator);
   });
 
-  useEffect(() => {
-    // Save content to localStorage whenever editorState changes
-    const contentState = editorState.getCurrentContent();
-    const contentStateJSON = JSON.stringify(convertToRaw(contentState));
-    localStorage.setItem("editorContent", contentStateJSON);
-  }, [editorState]);
+  // useEffect(() => {
+  //   // Save content to localStorage whenever editorState changes
+  //   const contentState = editorState.getCurrentContent();
+  //   const contentStateJSON = JSON.stringify(convertToRaw(contentState));
+  //   localStorage.setItem("editorContent", contentStateJSON);
+  // }, [editorState]);
 
   const handleKeyCommand = (command, editorState) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
@@ -70,36 +70,82 @@ const App = () => {
     return getDefaultKeyBinding(e);
   };
 
-  const handleBeforeInput = (char, editorState) => {
-    if (char === "#") {
+  const onEditorChange = (newEditorState) => {
+    setEditorState(newEditorState);
+
+    // Your logic to process the content goes here
+    const contentState = newEditorState.getCurrentContent();
+    const lastBlock = contentState.getLastBlock();
+    const lastBlockText = lastBlock.getText();
+    console.log("Current content:", lastBlockText);
+    setCurrentText(lastBlockText);
+  };
+
+  const handleBeforeInput = (input, editorState) => {
+    // console.log({ text });
+    // if (text === "# ") {
+    //   console.log("inside # ", text);
+    //   setEditorState(RichUtils.toggleBlockType(editorState, "header-one"));
+    //   return "handled";
+    // } else if (text === "* ") {
+    //   console.log("inside * ", text);
+    //   setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
+    //   return "handled";
+    // } else if (text === "** ") {
+    //   console.log("inside ** ", text);
+    //   setEditorState(RichUtils.toggleInlineStyle(editorState, "RED_COLOR"));
+    //   return "handled";
+    // } else if (text === "*** ") {
+    //   console.log("inside *** ", text);
+    //   setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
+    //   return "handled";
+    // }
+    // return "not-handled";
+
+    // const contentState = editorState.getCurrentContent();
+    // const selection = editorState.getSelection();
+    // const startKey = selection.getStartKey();
+    // const currentContentBlock = contentState.getBlockForKey(startKey);
+    // const text = currentContentBlock.getText();
+
+    console.log(currentText);
+
+    // Check if the input matches the desired patterns
+    if (currentText === "# " && input === " ") {
+      console.log("inside hash");
       setEditorState(RichUtils.toggleBlockType(editorState, "header-one"));
       return "handled";
-    } else if (char === "*") {
-      console.log("inside bold");
+    } else if (currentText === "* " && input === " ") {
+      console.log("inside * ");
       setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
       return "handled";
-    } else if (char === "**") {
-      console.log("inside red color");
+    } else if (currentText === "** " && input === " ") {
+      console.log("inside **");
       setEditorState(RichUtils.toggleInlineStyle(editorState, "RED_COLOR"));
       return "handled";
-    } else if (char === "***") {
+    } else if (currentText.endsWith("*** ") && input === " ") {
       setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
       return "handled";
     }
+
     return "not-handled";
   };
 
   const handlePastedText = (text, html, editorState) => {
-    if (text === "#") {
+    if (text === "# ") {
+      console.log("paste # ", text);
       setEditorState(RichUtils.toggleBlockType(editorState, "header-one"));
       return "handled";
-    } else if (text === "*") {
+    } else if (text === "* ") {
+      console.log("paste * ", text);
       setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
       return "handled";
-    } else if (text.trim() === "**") {
+    } else if (text === "** ") {
+      console.log("paste ** ", text);
       setEditorState(RichUtils.toggleInlineStyle(editorState, "RED_COLOR"));
       return "handled";
-    } else if (text.trim() === "***") {
+    } else if (text === "*** ") {
+      console.log("paste *** ", text);
       setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
       return "handled";
     }
@@ -107,19 +153,37 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div
+      style={{
+        maxWidth: "1100px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "30px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: "10px",
+        }}
+      >
+        <h3>Demo editor by Mohammad Jahid</h3>
+        <button className="save-button">Save</button>
+      </div>
       <div>
         <Editor
           editorState={editorState}
           handleKeyCommand={handleKeyCommand}
           keyBindingFn={mapKeyToEditorCommand}
-          handleBeforeInput={(char, editorState) =>
-            handleBeforeInput(char, editorState)
+          handleBeforeInput={(text, editorState) =>
+            handleBeforeInput(text, editorState)
           }
-          handlePastedText={(text, html, editorState) =>
-            handlePastedText(text, html, editorState)
+          handlePastedText={(text, editorState) =>
+            handlePastedText(text, editorState)
           }
-          onChange={(newEditorState) => setEditorState(newEditorState)}
+          onChange={(newEditorState) => onEditorChange(newEditorState)}
         />
       </div>
     </div>
